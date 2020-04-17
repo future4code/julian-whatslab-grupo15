7ff118e7e186bd87f3aad83e1dd4f00a0ef21d7d
Raphael Melo <raphaelmelo@users.noreply.github.com>
2020-04-17 12:36:46 -0300

    Feito layout do app.

--- whatslab15/src/App.js
+++ whatslab15/src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Mensagem-container">
+   
+      <div className="mensagem-canva">
+        <div className="mensagens-tela" ></div>
+        <div className="inputs-tela">
+          <input className="input-nome" onChange="" placeholder="Nome"/>
+          <input className="input-mensagem" onChange="" placeholder="Sua mensagem"/>
+          <button onChange="">Enviar</button>
+        </div>
+      </div>
+   
     </div>
   );
 }
